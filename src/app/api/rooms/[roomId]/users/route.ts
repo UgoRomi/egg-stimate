21c9d0d5e42a70e43fdd269b97d6dbfde1010736
Ugo Romi <ugo.romi@gmail.com>
2023-05-24 19:55:26 +0200

    added user deleting funcionality (turned off atm)

--- src/app/api/rooms/[roomId]/users/route.ts
+++ src/app/api/rooms/[roomId]/users/route.ts
@@ -9,3 +9,20 @@ export async function GET(request: NextRequest) {
     users: users.data || [],
   });
 }
+
+export async function DELETE(request: NextRequest) {
+  const urlParts = request.nextUrl.pathname.split('/');
+  const roomId = urlParts[urlParts.length - 2];
+  const userId = JSON.parse(request.cookies.get('user')?.value || '')?.id;
+  if (userId) {
+    await supabase.from('users').delete().eq('room', roomId).eq('id', userId);
+  }
+  const { data: remainingUsers } = await supabase
+    .from('users')
+    .select('*')
+    .eq('room', roomId);
+  // if there are 0 remaining users, delete the room
+  if (remainingUsers && remainingUsers.length === 0) {
+    supabase.from('rooms').delete().eq('id', roomId);
+  }
+}
