b2fb787a29f4881fd4c221a8a3f57bc048820aca
Ugo Romi <ugo.romi@gmail.com>
2023-06-05 00:18:56 +0200

    idk im tired

--- src/app/rooms/[roomId]/page.tsx
+++ src/app/rooms/[roomId]/page.tsx
@@ -19,18 +19,18 @@ export default function Page({ params }: { params: { roomId: string } }) {
       }
     },
   });
-  // useEffect(() => {
-  //   const callback = (ev: BeforeUnloadEvent) => {
-  //     removeRoomFromCookie();
-  //     fetch(`/api/rooms/${params.roomId}/users`, {
-  //       method: 'DELETE',
-  //     });
-  //   };
-  //   window.addEventListener('beforeunload', callback);
-  //   return () => {
-  //     window.removeEventListener('beforeunload', callback);
-  //   };
-  // }, [params.roomId]);
+  useEffect(() => {
+    const callback = async (ev: BeforeUnloadEvent) => {
+      removeRoomFromCookie();
+      await fetch(`/api/rooms/${params.roomId}/users`, {
+        method: 'DELETE',
+      });
+    };
+    window.addEventListener('beforeunload', callback);
+    return () => {
+      window.removeEventListener('beforeunload', callback);
+    };
+  }, [params.roomId]);
 
   return (
     <main className='flex-grow grid md:grid-cols-[3fr_2fr]'>
