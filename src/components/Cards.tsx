5f539d68b492085360102adfffe7148ad7da21d3
Ugo Romi <ugo.romi@gmail.com>
2023-06-04 23:32:32 +0200

    debounced vote

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,7 +1,11 @@
 'use client';
-import { showHideVotes, updateUser, vote } from '@/app/_actions';
+import {
+  showHideVotes,
+  updateUser as updateUserAction,
+  vote as voteAction,
+} from '@/app/_actions';
 import Image from 'next/image';
-import { useTransition } from 'react';
+import { useEffect, useState, useTransition } from 'react';
 import * as AspectRatio from '@radix-ui/react-aspect-ratio';
 import { cn, getRoomIdFromUrl, getUserFromCookie } from '@/lib/utils';
 import { useStore } from '@/lib/zustand';
@@ -9,13 +13,15 @@ import { Results } from './Results';
 import toast from 'react-hot-toast';
 import { Copy } from 'lucide-react';
 import { UserProfile } from './UserProfile';
+// @ts-expect-error missing types
+import { useDebounce } from '@uidotdev/usehooks';
 
 interface CardProps {
   value: number;
+  onClick: (value: number) => void;
 }
 
-const Card: React.FC<CardProps> = ({ value }) => {
-  let [, startTransition] = useTransition();
+const Card: React.FC<CardProps> = ({ value, onClick }) => {
   const users = useStore((state) => state.users);
   const updateUser = useStore((state) => state.updateUser);
   const userCookie = getUserFromCookie();
@@ -38,21 +44,8 @@ const Card: React.FC<CardProps> = ({ value }) => {
           isCurrentVote && 'w-[120px] h-[189px]'
         )}
         onClick={() => {
-          startTransition(() => vote(value));
           updateUser({ ...currentUser, current_vote: value });
-          // if every user has voted, toggle the votes
-          if (
-            Array.from(users.values()).every(
-              (user) =>
-                user.current_vote !== null ||
-                user.id === currentUser?.id ||
-                user.is_spectator
-            )
-          ) {
-            const roomId = getRoomIdFromUrl();
-            if (!roomId) return;
-            startTransition(() => showHideVotes(roomId, true));
-          }
+          onClick(value);
         }}
       >
         <AspectRatio.Root ratio={93 / 146}>
@@ -77,6 +70,35 @@ export function Cards() {
   const spectators = useStore((state) =>
     Array.from(state.users.values()).filter((user) => user.is_spectator)
   );
+
+  const [vote, setVote] = useState<number | undefined>(undefined);
+  const debouncedVote = useDebounce(vote, 500);
+  useEffect(() => {
+    const updateVote = async () => {
+      if (debouncedVote) {
+        startTransition(() => voteAction(debouncedVote));
+        // if every user has voted, toggle the votes
+        if (
+          Array.from(users.values()).every(
+            (user) =>
+              user.current_vote !== null ||
+              user.id === currentUser?.id ||
+              user.is_spectator
+          )
+        ) {
+          const roomId = getRoomIdFromUrl();
+          if (!roomId) return;
+          startTransition(() => showHideVotes(roomId, true));
+        }
+      }
+    };
+
+    updateVote();
+  }, [debouncedVote, currentUser?.id, users]);
+
+  if (!currentUser) {
+    return null;
+  }
 
   return (
     <div className='bg-white p-4 flex items-center flex-col'>
@@ -120,11 +142,11 @@ export function Cards() {
                 const formData = new FormData();
                 formData.append(
                   'is_spectator',
-                  (!currentUser.is_spectator).toString()
+                  currentUser.is_spectator ? 'off' : 'on'
                 );
                 formData.append('name', currentUser.name);
                 formData.append('userId', currentUser.id.toString());
-                startTransition(() => updateUser(formData));
+                startTransition(() => updateUserAction(formData));
               }}
               className='rounded-full bg-orange-500 cursor-pointer px-5 py-3 text-md text-white shadow-sm hover:bg-orange-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500 disabled:opacity-50 disabled:cursor-not-allowed'
             >
@@ -134,18 +156,18 @@ export function Cards() {
         ) : (
           <div className='grid grid-cols-[repeat(3,100px)] gap-x-7 gap-y-2'>
             <p className='col-span-3'>Scegli la tua carta 👇🏻</p>
-            <Card value={0} />
-            <Card value={1} />
-            <Card value={2} />
-            <Card value={3} />
-            <Card value={4} />
-            <Card value={5} />
-            <Card value={6} />
-            <Card value={7} />
-            <Card value={8} />
-            <Card value={9} />
-            <Card value={10} />
-            <Card value={11} />
+            <Card value={0} onClick={setVote} />
+            <Card value={1} onClick={setVote} />
+            <Card value={2} onClick={setVote} />
+            <Card value={3} onClick={setVote} />
+            <Card value={4} onClick={setVote} />
+            <Card value={5} onClick={setVote} />
+            <Card value={6} onClick={setVote} />
+            <Card value={7} onClick={setVote} />
+            <Card value={8} onClick={setVote} />
+            <Card value={9} onClick={setVote} />
+            <Card value={10} onClick={setVote} />
+            <Card value={11} onClick={setVote} />
           </div>
         )}
       </div>
